c44142b1b1d95c0c41be2f99b23918b786fb2791
jalbiston <jacob.albiston@gmail.com>
2021-10-29 16:46:41 -0600

    edit functionality

--- src/Edit.js
+++ src/Edit.js
@@ -24,20 +24,40 @@ function Edit(props) {
                         
                     
                     >
-                        <label> Edit First Name: <input type="text" className="inputBox" placeholder={item.name.first} /></label>
+                        <label> Edit First Name: <input type="text" className="inputBox" placeholder={item.name.first} onChange = {(e) =>{
+                           data[item.id -1].name.first = e.target.value
+                            
+                        }}
+                        
+                        /></label>
                         
                         
-                        <label> Edit Last Name: <input type= "text"  className="inputBox" placeholder={item.name.last}/></label>
+                        <label> Edit Last Name: <input type= "text"  className="inputBox" placeholder={item.name.last} onChange ={(e) =>{
+                            data[item.id -1].name.last = e.target.value
+                        }}
+                        
+                        /></label>
                         
                             
                     
-                        <label> Edit City: <input type= "text"  className="inputBox" placeholder={item.city}/></label>
+                        <label> Edit City: <input type= "text"  className="inputBox" placeholder={item.city} onChange ={(e) =>{
+                            data[item.id -1].city = e.target.value
+                        }}
+                        
+                        /></label>
                          
                     
-                        <label> Edit Job: <input type="text" className="inputBox" placeholder={item.title}/></label>
+                        <label> Edit Job: <input type="text" className="inputBox" placeholder={item.title} onChange={(e) =>{
+                            data[item.id -1].title = e.target.value
+                        }}
+                        
+                        /></label>
                         
                      
-                        <label> Edit Employer:  <input type="text" className="inputBox" placeholder={item.employer}/></label>
+                        <label> Edit Employer:  <input type="text" className="inputBox" placeholder={item.employer} onChange={(e) =>{
+                            data[item.id -1].employer = e.target.value
+                        }}
+                         /></label>
                         
                     <br/>
                     
@@ -45,13 +65,24 @@ function Edit(props) {
 
                     <label> Edit Favorite Movies: 
                     <ol className ="items">
-                        <input type="text" className="inputBox" placeholder={item.favoriteMovies[0]} />
+                        <input type="text" className="inputBox" placeholder={item.favoriteMovies[0]} onChange={(e) => {
+                            data[item.id -1].favoriteMovies[0] = e.target.value
+                        }}
+                         />
                         
 
-                        <input type="text" className="inputBox" placeholder={item.favoriteMovies[1]} />
+                        <input type="text" className="inputBox" placeholder={item.favoriteMovies[1]} onChange={(e) => {
+                            data[item.id -1].favoriteMovies[1] =e.target.value
+                        }}
+                        
+                        />
                         
 
-                        <input type="text" className="inputBox" placeholder={item.favoriteMovies[2]}/>
+                        <input type="text" className="inputBox" placeholder={item.favoriteMovies[2]} onChange={(e) => {
+                            data[item.id -1].favoriteMovies[2] = e.target.value
+                        }}
+                        
+                        />
                         
                     </ol>
 
