8ddba62f837144a26d7bb7337898f0bc4955e49e
jalbiston <jacob.albiston@gmail.com>
2021-10-29 13:42:10 -0600

    fine tuning

--- src/Structure.js
+++ src/Structure.js
@@ -3,14 +3,17 @@ import { MdArrowBackIosNew} from 'react-icons/md'
 import {MdArrowForwardIos} from 'react-icons/md'
 import Functionality from "./Functionality"
 import React, { useState } from 'react'
-
+import {data} from "./data"
 
 const Structure = () => {
   
   let [cardNumber, setCardNumber] = useState(1)
   
 console.log(cardNumber)
-    return (
+    
+
+
+        return (
     <div className="App">
      <header>
        <h1> Home</h1>
@@ -21,7 +24,13 @@ console.log(cardNumber)
         <Functionality cardNumber = {cardNumber}/>
         <div className="controls">
             <h3 className="previous-h3"
-            onClick ={() => setCardNumber(cardNumber - 1)}
+            onClick ={() => {
+              if(cardNumber <= 1){
+                alert("You can't have a negative card, idiot.")
+              } else {
+                setCardNumber(cardNumber -1)
+              }
+            }}
             > <MdArrowBackIosNew /> Previous </h3>
             <div className ='control-btns'>
               <button>Edit</button>
@@ -29,7 +38,16 @@ console.log(cardNumber)
               <button>New</button>
             </div>
             <h3 className="next-h3"
-            onClick ={() => setCardNumber(cardNumber + 1)}
+            onClick ={()  => {
+            if(cardNumber >= data.length){
+              // stop moving the cards
+              alert("There No more cards Foo")
+            } else {
+              // allow user to keep going
+              setCardNumber(cardNumber +1)
+            }
+
+            }}
             > Next <MdArrowForwardIos />
             </h3>
           </div>
